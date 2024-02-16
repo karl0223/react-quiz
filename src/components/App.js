c7c4913bcb9331ea45adb2fb80b91310a0036a6a
karl ducta <karlducta022@gmail.com>
2024-02-17 00:29:11 +0800

    created Question component

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
 
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -27,13 +29,19 @@ function reducer(state, action) {
         ...state,
         status: "error",
       };
+
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Unhandled action type: " + action.type);
   }
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -49,7 +57,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
