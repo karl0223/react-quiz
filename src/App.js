c17cd6fea4f97f4b2e1b9f44f8e2c1bad71f9dc7
karl ducta <karlducta022@gmail.com>
2024-02-16 16:22:27 +0800

    installing fake api with json-server, Setting up the UI and load the questions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,56 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataError":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Unhandled action type: " + action.type);
+  }
+}
 
 export default function App() {
-  return <DateCounter />;
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataError" }));
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+      </Main>
+    </div>
+  );
 }
